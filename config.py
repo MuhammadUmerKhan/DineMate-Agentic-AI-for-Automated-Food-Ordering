398d671afc78933f4a561bbd22f79404d41f6a0a
Muhammad Umer Khan <muhammadumerk546@gmail.com>
2025-03-08 16:02:27 +0500

    sqllite database

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+import sqlite3
 
 load_dotenv()  # Load environment variables from .env
 
@@ -12,3 +13,12 @@ DB_CONFIG = {
     "database": os.getenv("DB_NAME"),
     "port": int(os.getenv("DB_PORT", 3306)),
 }
+
+DB_PATH = "./foodbot.db"
+def get_db_connection():
+    """Create and return a database connection to SQLite."""
+    conn = sqlite3.connect(DB_PATH)
+    conn.row_factory = sqlite3.Row  # ✅ Allows accessing columns by name
+    return conn
+
+# print(get_db_connection())
