363b3d37584f1f879662423d56b560b7c0c2cf35
Muhammad Umer Khan <muhammadumerk546@gmail.com>
2025-07-04 23:40:49 +0500

    del

--- app/MYSQL_Pages/add_users.py
+++
@@ -1,8 +0,0 @@
-from database.MySQL_db import Database
-
-db = Database()
-
-# ✅ Add admin, kitchen staff, and customer support users
-print(db.add_user("admin", "admin123", "admin"))
-print(db.add_user("chef", "chef123", "kitchen_staff"))
-print(db.add_user("support", "support123", "customer_support"))
